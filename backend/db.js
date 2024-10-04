e064d0fb638bc67052b14832b92bfbf852f7f738
Syed Sultan <syedsultanmohiuddin0786@gmail.com>
2024-10-04 07:55:41 +0530

    Added accounts to the database

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,25 @@
 const mongoose = require("mongoose");
 mongoose.connect("mongodb+srv://sultan112:KdpEilSZ1ixxlOxN@cluster0.8pv9q3r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 
+// User Schema
 const userSchema = new mongoose.Schema({
-    userName:String,
+    userName: String,
     password: String,
     firstName: String,
     lastName: String
-})
+});
 
-const User = mongoose.model("User",userSchema);
+// Account Schema
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'  // Correctly referencing the 'User' model as a string
+    },
+    balance: Number
+});
 
-module.exports = User;
+// Models
+const User = mongoose.model("User", userSchema);
+const Account = mongoose.model("Account", accountSchema);
+
+module.exports = { User, Account };
